0da001484174be8d89ae18925c373a3a3f0d7159
Benedict Lawrence <BTLawrence44@gmail.com>
2021-06-02 16:04:59 +0100

    Jest testing

--- constructor/chitter.js
+++ constructor/chitter.js
@@ -2,7 +2,12 @@ const {Client} = require('pg')
 const sgMail = require('@sendgrid/mail') 
  
 
-    let client;  
+let client;    
+
+const prod = new Client({
+    "port": 5432, 
+    "database": "chitter_js"
+}); 
  
 
 class soicalMedia {     
@@ -10,29 +15,25 @@ class soicalMedia {
 
     async start() { 
         const soical = new soicalMedia 
-        await soical.connect()
+        await soical.connect() 
+        return true
     }
 
     async connect() {
         try {    
             if (process.env.NODE_ENV === 'test') {   
-
-                console.log('connecting to test_database')   
-                const test = new Client({
-                    "port": 5432, 
-                    "database": "chitter_js_test"
-                });   
-                await test.connect()    
-                
-                client = test 
+                    console.log('connecting to test_database')    
+                    const test = new Client({
+                        "port": 5432, 
+                        "database": "chitter_js_test"
+                    }); 
+                    await test.connect()    
+                    client = test    
+                return true
             } else { 
-                console.log('connecting to database')  
-                const prod = new Client({
-                    "port": 5432, 
-                    "database": "chitter_js"
-                }); 
-                await prod.connect()     
-                client = prod 
+                    console.log('connecting to database')  
+                    await prod.connect()     
+                    client = prod   
             } 
         } 
         catch(e) {
@@ -53,12 +54,13 @@ class soicalMedia {
 
     async newChitter(peep, id) {  
         try{ 
-            await client.query('INSERT INTO chitter_messages (user_id, message, created_on, chitter_profile) VALUES(DEFAULT, $1, current_timestamp, $2)', [peep, id])   
-            await Media.email(peep) 
+            await client.query('INSERT INTO chitter_messages (user_id, message, created_on, chitter_profile) VALUES(DEFAULT, $1, current_timestamp, $2)', [peep, id])    
+            // this is the email method 
+            // await Media.email(peep) 
             return true
         }
         catch(e) { 
-            console.log(e)
+            // console.log(e)
             return false 
         }
     }  
@@ -70,7 +72,7 @@ class soicalMedia {
             return true
         } 
         catch(e){ 
-            console.log(e)
+            // console.log(e)
             return false; 
         }
     } 
@@ -102,57 +104,56 @@ class soicalMedia {
             }  
         }
         catch(e) {
-            console.log(e)  
             return false
         }
     } 
  
-    async email(peep) { 
-        // for some resion async doesent work in this funaction
-        try {
-            peep.split(' ').forEach(async item => {
-                if (item.includes('@')) { 
-                    const email = await client.query('SELECT email FROM chitter_profile WHERE name = $1', [item.substring(1)])   
-                    await Media.send_email(email.rows)    
-                    return true
-                }  
-            }) 
-        }
-        catch(e) {
-            console.log(e) 
-            return false
-        } 
-    }   
+    // async email(peep) { 
+    //     // for some resion async doesent work in this funaction
+    //     try {
+    //         peep.split(' ').forEach(async item => {
+    //             if (item.includes('@')) { 
+    //                 const email = await client.query('SELECT email FROM chitter_profile WHERE name = $1', [item.substring(1)])   
+    //                 await Media.send_email(email.rows)    
+    //                 return true
+    //             }  
+    //         }) 
+    //     }
+    //     catch(e) {
+    //         console.log(e) 
+    //         return false
+    //     } 
+    // }   
     
     
-    async send_email(email) {
-        try {
-            console.log(email)  
-            console.log(process.env.SENDGRID_API_KEY) 
-            sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-            const msg = { 
-                // email address
-                to: '', 
-                from: '',
-                subject: 'Someone is talking about you on chitter',
-                text: "Login in to chitter and see who's talking about",
-                html: '<strong>and easy to do anywhere, even with Node.js</strong>', 
-            } 
-            sgMail
-            .send(msg)
-            .then(() => {
-            console.log('Email sent')
-            }) 
-            .catch((error) => {
-                console.error(error.response.body)
-            }) 
-            return true  
-        } 
-        catch(e) {
-            console.log(e) 
-            return false
-        }
-    }
+    // async send_email(email) {
+    //     try {
+    //         console.log(email)  
+    //         console.log(process.env.SENDGRID_API_KEY) 
+    //         sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+    //         const msg = { 
+    //             // email address
+    //             to: '', 
+    //             from: '',
+    //             subject: 'Someone is talking about you on chitter',
+    //             text: "Login in to chitter and see who's talking about",
+    //             html: '<strong>and easy to do anywhere, even with Node.js</strong>', 
+    //         } 
+    //         sgMail
+    //         .send(msg)
+    //         .then(() => {
+    //         console.log('Email sent')
+    //         }) 
+    //         .catch((error) => {
+    //             console.error(error.response.body)
+    //         }) 
+    //         return true  
+    //     } 
+    //     catch(e) {
+    //         console.log(e) 
+    //         return false
+    //     }
+    // }
 
 
 }  
@@ -164,3 +165,5 @@ module.exports = soicalMedia
 
 
 //to run the web app npm run devStart 
+
+//for test coverage ( npx jest --coverage )
--- spec/app.test.js
+++ spec/app.test.js
@@ -1,55 +1,125 @@
 // import { response } from 'express'
 // import supertest from 'supertest' 
-// import app from '../app.js'   
+// import app from '../app.js'    
+
+
+
+const {Client} = require('pg')   
 
 const set_database = require('./setup_database') 
 
 set_database
 
-const soicalMedia = require('../constructor/chitter')  
+const soicalMedia = require('../constructor/chitter')   
 
-describe('test peeps on Chitter', async() => {
 
-    test('Out putting all Peep inside the database', async() =>{
-        Media = new soicalMedia  
-        await Media.connect() 
-        await Media.newChitter('This is my first message in JS', 1) 
-        await Media.newChitter('Working with express this time', 2)  
-        await Media.newChitter('Working out how to use SQL in JS', 3)   
-        const result = await Media.allChitter()
-        expect( result.map((item) => (item.message))).toEqual(['This is my first message in JS', 'Working with express this time', 'Working out how to use SQL in JS']) 
+describe('connecting to test database', async() => {
+    test('in application', async() => {
+        Media = new soicalMedia   
+        expect( await Media.start() ).toBe(true)
     })
+})
+
+
+describe('peeps', async() => {
+
+    test('Out put all Peeps inside the database', async() =>{
+        Media = new soicalMedia  
+        await Media.connect()   
+        // await was taken from here
+        Media.newChitter('This is my first message in JS', 1) 
+        Media.newChitter('Working with express this time', 2)  
+        Media.newChitter('Working out how to use SQL in JS', 3)   
+        const result = await Media.allChitter()  
+        expect( result.map((item) => (item.message))).toEqual(['This is my first message in JS', 'Working with express this time', 'Working out how to use SQL in JS']) 
+    }, 1000) 
 
     test('adding a new peep to Chitter', async() => {
         Media = new soicalMedia 
-        await Media.connect() 
         await Media.newChitter('Testing my app with Jest and Jasmine', 4)  
         const result = await Media.allChitter()
         expect( result.map((item) => (item.message))).toEqual(["This is my first message in JS", "Working with express this time", "Working out how to use SQL in JS", "Testing my app with Jest and Jasmine"]) 
-    }) 
+    }, 1000) 
 
     test('deleting peep from chitter', async() => {
         Media = new soicalMedia 
-        await Media.connect() 
         await Media.deleteChitter(1) 
         const result = await Media.allChitter() 
         expect( result.map((item) => (item.message))).toEqual(["Working with express this time", "Working out how to use SQL in JS", "Testing my app with Jest and Jasmine"])
+    }, 1000) 
+
+    // test('rase an error create new peep when id is missing', async() => { 
+    //     Media = new soicalMedia 
+    //     expect(await Media.allChitter('hi')).toBe([])
+    // }) 
+
+    test('raise error if chitter delete has the wrong input', async() => {
+        Media = new soicalMedia  
+        expect(await Media.deleteChitter('')).toBe(false)
+    }) 
+
+    test('raise error if you chitter has the wrong input', async() => { 
+        Media = new soicalMedia
+        expect( await Media.newChitter(null, '')).toBe(false) 
     })
 
-}) 
+})
 
-describe('test chitters profile and login', async() => {
-    test('Looging in to chitter account', async() => {
+describe('Chitter login and logging to profile', async() => {
+
+    test('Should create a new user profile', async() => {
         Media = new soicalMedia 
-        await Media.connect() 
-        await new_chitter_account('Jo_heart', 'Jo_heart@gmail.com', '123Build*')
-        await new_chitter_account('Tom_scott', 'Tome_scote@gmail.com', 'happy_me_123') 
-        await new_chitter_account('you_found', 'me@gmail.com', 'well_done_123') 
-        const result = await client.query('SELECT * FROM chitter_profile;') 
-        const chitter_accounts = result.rows.map((item) => ( {profile_id: item.profile_id, name: item.name, email: item.email, password: item.password} ))    
-        chitter_accounts
-        // work on here tomorrow, this will be interasting 
+        await Media.new_chitter_account('Jo_heart', 'Jo_heart@gmail.com', '123Build*')
+        await Media.new_chitter_account('Tom_scott', 'Tome_scote@gmail.com', 'happy_me_123') 
+        await Media.new_chitter_account('you_found', 'me@gmail.com', 'well_done_123')  
+
+        const test = new Client({
+            "port": 5432, 
+            "database": "chitter_js_test"
+        });    
+
+        await test.connect()  
+        let client = test
+
+        const result = await client.query('SELECT * FROM chitter_profile;')  
+
+
+        const chitter_accounts = result.rows.map((item) => ( {profile_id: item.profile_id, name: item.name, email: item.email, password: item.password} ))      
+
+        expect(chitter_accounts.map((item) => item.name)).toEqual(['Jo_heart', 'Tom_scott', 'you_found'])
+        expect(chitter_accounts.map((item) => item.email)).toEqual(['Jo_heart@gmail.com', 'Tome_scote@gmail.com', 'me@gmail.com'])
+        expect(chitter_accounts.map((item) => item.password)).toEqual(['123Build*', 'happy_me_123', 'well_done_123'])
+    }) 
+
+    test('rase error if chitter input is wrong', async() => {
+        Media = new soicalMedia  
+        expect(await Media.new_chitter_account(null, null, null)).toBe(false)
+        
     })
+
+    test('error will appear if another user has the same name', async() => {
+        Media = new soicalMedia 
+        expect(await Media.new_chitter_account('Jo_heart', 'Jo_Smith@gmail.com', '123Build*')).toEqual('error')
+    }) 
+
+    test('error will appear if another user has the same email', async() => {
+        Media = new soicalMedia  
+        expect(await Media.new_chitter_account('Smith', 'Jo_heart@gmail.com', '123Build*')).toEqual('error')
+    })  
+
+    test('Logging into Chitter app', async() => {
+        Media = new soicalMedia  
+        const result = await Media.logging_in('Jo_heart', '123Build*')   
+        let Login_result = result.rows.map((item) => item.profile_id)
+        expect(Login_result).toEqual([1])
+    })   
+
+    test('unidetifyed user login', async() => {
+        Media = new soicalMedia  
+        expect(await Media.logging_in('not_me', '123Build*')).toEqual('error')
+        expect(await Media.logging_in('Jo_heart', '123')).toEqual('error')
+    })  
+
 })
 
     //npm test --detectOpenHandles
